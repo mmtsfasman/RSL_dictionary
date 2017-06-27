7bbbc03dcd7d688ad0eb03ed7bf3525e80a59cdd
Maria Tsfasman <mmtsfasman@gmail.com>
2017-06-27 19:04:19 +0200

    --
    всё равно не работает (

--- dict.py
+++ dict.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import re
+import sqlite3
+from datetime import datetime
 
 app = Flask (__name__)
 
@@ -7,16 +9,17 @@ app = Flask (__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search')
+@app.route('/search',  methods=['post'])
 def search():
-    data = open('data.csv','r', encoding = 'UTF-8')
-    results = []
+    #text = request.form['text'] #
+    data = open('data.csv','r', encoding = 'UTF-8').read()
+    conn = sqlite3.connect('data.csv')
+    results = {}
     results_num = 0
     for l in data:
         if request.values['request'] in l:
             results.append(l)
             results_num += 1
-
     return render_template('search.html', results = results, results_num = results_num)
 
 @app.route('/word/<word>')
@@ -25,7 +28,7 @@ def signpage():
     for l in data:
         if request.values['RSL_word'] in l:
             results.append(l)
-    return render_template('search.html', results = results, word = id_word)
+    return render_template('word.html', results = results, word = id_word)
 
 
 if __name__ == '__main__':

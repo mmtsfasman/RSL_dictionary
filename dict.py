2cc48e9416bbc67d85335dad93a90e3fca34a1f5
Maria Tsfasman <mmtsfasman@gmail.com>
2017-07-28 22:13:01 +0300

    Доведение поиска и начальной страницы до рабочего состояния

--- dict.py
+++ dict.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, url_for
 import re
 import sqlite3
-from datetime import datetime
+import csv
+
 
 app = Flask (__name__)
 
@@ -9,26 +10,31 @@ app = Flask (__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search',  methods=['post'])
+@app.route('/search',  methods=['get'])
 def search():
-    #text = request.form['text'] #
-    data = open('data.csv','r', encoding = 'UTF-8').read()
-    conn = sqlite3.connect('data.csv')
-    results = {}
+    #text = request.form['text']
+    data = open('data.csv','r', encoding = 'UTF-8')
+    #conn = sqlite3.connect('data.csv')
+    results = []
     results_num = 0
     for l in data:
         if request.values['request'] in l:
-            results.append(l)
+            attr = l.split(';')
+            word_info = []
+            for info in attr:
+                word_info.append(info)
+            results.append(word_info)
             results_num += 1
     return render_template('search.html', results = results, results_num = results_num)
 
 @app.route('/word/<word>')
-def signpage():
+def word(word):
     data = open('data.csv','r', encoding = 'UTF-8')
+    word_info = []
     for l in data:
-        if request.values['RSL_word'] in l:
-            results.append(l)
-    return render_template('word.html', results = results, word = id_word)
+        if word in l:
+            word_info = l.split(';')
+    return render_template('word.html', results = word_info)
 
 
 if __name__ == '__main__':

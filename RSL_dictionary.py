a5001003b0e25e7b2c4e9ea4fc0e40dbd155491c
mmtsfasman <mmtsfasman@gmail.com>
2017-12-05 22:36:38 +0300

    everything updated

--- RSL_dictionary.py
+++ RSL_dictionary.py
@@ -16,25 +16,21 @@ def index():
 
 @app.route('/search',  methods=['get'])
 def search():
-    #text = request.form['text']
-    with open('data.csv', encoding='utf-8') as csvfile:
-        data = csv.DictReader(csvfile)
-        results = {}
-        results_num = 0
-        for n, entry in enumerate(data):
-            #??????
-            if entry != None and request.values['query'] in entry.homograph[0].lexical_unit[0].translation or request.values['query'] in ([sign.hamnosys for sign in entry.signs]):
-                results[n] = entry
-                results_num += 1
-    return render_template('search.html', results = results, results_num = results_num)
+    results = {}
+    results_num = 0
+    for n, entry in enumerate(data):        
+        if entry != None and (request.values['query'] in (' '.join([[lu.translation for lu in homograph.lexical_units] for homograph in entry.homographs][0])) or request.values['query'] in (' '.join([sign.hamnosys for sign in entry.signs]))):
+            results[n] = entry
+            results_num += 1
+    return render_template('search.html', results = results, results_num = results_num, query = request.values['query'] )
 
-@app.route('/edit', methods=['post'])
+'''@app.route('/edit', methods=['post'])
 
 @app.route('/save', methods=['post'])
 def save():
     
     
-    return redirect
+    return redirect'''
 
 @app.route('/word/<int:word_n>')
 def word(word_n):
